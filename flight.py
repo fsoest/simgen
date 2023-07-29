8669876898df25f680f8caeef31e567c61a38ecc
Felix Soest <soestfelix@gmail.com>
2023-07-29 15:04:45 +0200

    Empty star definition, add Mode S cap. to aircraft type

--- flight.py
+++ flight.py
@@ -6,7 +6,7 @@ class Flight:
         self.route = random.choice(routes[fix]['routes'])
         self.flight = flight
         self.callsign = self.flight[0]
-        self.acft_type = self.flight[2][:-1]
+        self.acft_type = self.flight[2][:-1] + '/L'
         self.dep_airport = self.flight[1]
         self.dep_airport = dep
         self.arr_airport = arr
@@ -19,14 +19,17 @@ class Flight:
         self.lat = entry_points[fix][0]
         self.lon = entry_points[fix][1]
 
-    def make_entry(self):
+    def make_entry(self, rfl='390'):
         entry = ''
         entry += ':'.join(['@N', self.callsign, str(self.squawk), '1', self.lat, self.lon, self.lvl, '0', self.heading, '0\n'])
         entry += ':'.join(['$FP{0}'.format(self.callsign), '*A', 'I', self.acft_type, str(300), self.dep_airport, \
-                           '1000', '1000', '390', self.arr_airport, '2', '50', '4', '00', 'EDDL', '/V/', self.route])
+                           '1000', '1000', rfl, self.arr_airport, '2', '50', '4', '00', 'EDDL', '/V/', self.route])
         entry += '\n'
         # entry += '$ROUTE:{0}\nSTART:{1}\nREQALT:{2}\n'.format(self.route, self.start, self.reqalt)
         entry += 'START:{0}\nREQALT:{1}\n'.format(self.start, self.reqalt)
         entry += 'INITIALPSEUDOPILOT:{0}\n'.format(self.pseudo)
         entry += 'SIMDATA:{}:*:*:25:3:0\n\n'.format(self.callsign)
         return entry
+
+    def star(self):
+        pass
